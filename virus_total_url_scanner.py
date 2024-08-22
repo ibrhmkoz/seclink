ff9e9b18771e6cec7043c98afc53c4492b2b0452
ibrahimkoz <ibrahimkoz@outlook.com>
2024-08-22 17:49:40 +0300

    Update VirusTotalURLScanner to use APIv3

--- virus_total_url_scanner.py
+++ virus_total_url_scanner.py
@@ -7,32 +7,35 @@ import certifi
 class VirusTotalURLScanner:
     def __init__(self, api_key):
         self._api_key = api_key
-        self._scan_url = 'https://www.virustotal.com/vtapi/v2/url/scan'
-        self._report_url = 'https://www.virustotal.com/vtapi/v2/url/report'
+        self._base_url = 'https://www.virustotal.com/api/v3'
+        self._headers = {
+            'x-apikey': self._api_key,
+            'Accept': 'application/json'
+        }
 
     async def scan_url(self, url):
         ssl_context = ssl.create_default_context(cafile=certifi.where())
         async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl_context)) as session:
-            scan_params = {'apikey': self._api_key, 'url': url}
-            async with session.post(self._scan_url, data=scan_params) as response:
+            scan_data = {'url': url}
+            async with session.post(f'{self._base_url}/urls', headers=self._headers, data=scan_data) as response:
                 scan_result = await response.json()
 
-            scan_id = scan_result.get('scan_id')
-            if not scan_id:
-                raise ValueError(f"Failed to get scan_id. Response: {scan_result}")
+            analysis_id = scan_result.get('data', {}).get('id')
+            if not analysis_id:
+                raise ValueError(f"Failed to get analysis_id. Response: {scan_result}")
 
-            return await self.poll_result(session, scan_id)
+            return await self.poll_result(session, analysis_id)
 
-    async def poll_result(self, session, scan_id):
+    async def poll_result(self, session, analysis_id):
         max_tries = 10
         base_wait_time = 1
 
         for attempt in range(max_tries):
-            report_params = {'apikey': self._api_key, 'resource': scan_id}
-            async with session.get(self._report_url, params=report_params) as response:
+            async with session.get(f'{self._base_url}/analyses/{analysis_id}', headers=self._headers) as response:
                 report_result = await response.json()
 
-            if report_result.get('response_code') == 1:  # Result is ready
+            status = report_result.get('data', {}).get('attributes', {}).get('status')
+            if status == 'completed':
                 return report_result
 
             # If result not ready, wait before trying again
