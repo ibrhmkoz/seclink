16d8a918b1003a72d495a909b2aa18604b232e8b
ibrahimkoz <ibrahimkoz@outlook.com>
2024-08-23 16:33:10 +0300

    Move the extract and aggregate results logic into another unit called VirusTotalThoroughScanner

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
 import asyncio
-
-from flask import Flask, Blueprint, request, jsonify, render_template
+from flask import Flask, Blueprint, request, jsonify
 from flask.views import MethodView
 
-from extract_links import extract_links
+from virus_total_thorough_scanner import VirusTotalThoroughScanner
 from virus_total_url_scanner import VirusTotalURLScanner, CachedScanner
 
 app = Flask(__name__)
 v1 = Blueprint('v1', __name__, url_prefix='/api/v1')
-
-
-async def format_to_tabular_form(links, results):
-    formatted_results = []
-
-    for link, result in zip(links, results):
-        formatted_results.append(dict(link, **result["data"]["attributes"]["stats"]))
-
-    return formatted_results
 
 
 class ScannerView(MethodView):
@@ -29,26 +19,14 @@ class ScannerView(MethodView):
             return jsonify({"error": "URL not provided in the payload"}), 400
 
         url = data['url']
-        links = extract_links(url)
 
-        # Create a list of coroutines
-        scan_tasks = [self.scanner.scan_url(link) for link in links]
-
-        # Run all scan tasks concurrently
-        results = await asyncio.gather(*scan_tasks)
-
-        # Create a list of dictionaries with link and result
-        tabular_form = await format_to_tabular_form(links, results)
-
-        return
-
-
-    async def get(self):
-        return render_template("scan_result.html")
+        return await self.scanner.scan_url(url)
 
 
 VIRUS_TOTAL_API_KEY = '22d7b8b2228b98184fc9416de2a51bae89d987dbcd6d3f56ac1992616c7156a2'
-scanner_view = ScannerView.as_view('scanner', CachedScanner(VirusTotalURLScanner(VIRUS_TOTAL_API_KEY)))
+
+scan_result_summarizer = VirusTotalThoroughScanner(CachedScanner(VirusTotalURLScanner(VIRUS_TOTAL_API_KEY)))
+scanner_view = ScannerView.as_view('scanner', scan_result_summarizer)
 v1.add_url_rule('/scan', view_func=scanner_view)
 
 if __name__ == "__main__":
---
+++ virus_total_thorough_scanner.py
@@ -0,0 +1,35 @@
+import asyncio
+
+from flask import jsonify
+
+from extract_links import extract_links
+
+
+def format_to_tabular_form(links, results):
+    formatted_results = []
+
+    for link, result in zip(links, results):
+        formatted_results.append(dict(link, **result["data"]["attributes"]["stats"]))
+
+    return formatted_results
+
+
+# VirusTotalThoroughScanner scans a URL and its direct links, aggregates the results, and returns them.
+class VirusTotalThoroughScanner:
+    def __init__(self, scanner):
+        self.scanner = scanner
+
+    async def scan_url(self, url):
+        # Create a list of dictionaries with link and result
+        links = extract_links(url)
+
+        # Create a list of coroutines
+        scan_tasks = [self.scanner.scan_url(link) for link in links]
+
+        # Run all scan tasks concurrently
+        results = await asyncio.gather(*scan_tasks)
+
+        # Create a list of dictionaries with link and result
+        formatted_results = [{"link": link, "result": result} for link, result in zip(links, results)]
+
+        return formatted_results
