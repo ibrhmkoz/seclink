76bb9c2a1493f2e798266f4ed5e5ba30dee00d43
ibrahimkoz <ibrahimkoz@outlook.com>
2024-08-24 15:39:04 +0300

    Add results.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask.views import MethodView
 
-from virus_total_thorough_scanner import VirusTotalThoroughScanner
+from virus_total_thorough_scanner import VirusTotalThoroughScanner, StubScanner
 from virus_total_url_scanner import VirusTotalURLScanner, CachedScanner
 
 
@@ -10,13 +10,14 @@ class ScannerView(MethodView):
         self.scanner = scanner
 
     async def post(self):
-        data = request.json
-        if not data or 'url' not in data:
-            return jsonify({"error": "URL not provided in the payload"}), 400
+        url = request.form.get('url')
+        print(url)
+        if url is None:
+            return jsonify({'error': 'URL is required'}), 400
 
-        url = data['url']
+        result = await self.scanner.scan_url(url)
 
-        return await self.scanner.scan_url(url)
+        return render_template('results.html', results=result)
 
     async def get(self):
         return render_template('index.html')
@@ -26,8 +27,11 @@ app = Flask(__name__)
 
 VIRUS_TOTAL_API_KEY = '22d7b8b2228b98184fc9416de2a51bae89d987dbcd6d3f56ac1992616c7156a2'
 
-scan_result_summarizer = VirusTotalThoroughScanner(CachedScanner(VirusTotalURLScanner(VIRUS_TOTAL_API_KEY)))
-scanner_view = ScannerView.as_view('scanner', scan_result_summarizer)
+virus_total_thorough_scanner = VirusTotalThoroughScanner(CachedScanner(VirusTotalURLScanner(VIRUS_TOTAL_API_KEY)))
+
+stub_virus_total_thorough_scanner = StubScanner()
+
+scanner_view = ScannerView.as_view('scanner', stub_virus_total_thorough_scanner)
 app.add_url_rule('/', view_func=scanner_view)
 
 if __name__ == "__main__":
--- virus_total_thorough_scanner.py
+++ virus_total_thorough_scanner.py
@@ -3,15 +3,6 @@ import asyncio
 from extract_links import extract_links
 
 
-def format_to_tabular_form(links, results):
-    formatted_results = []
-
-    for link, result in zip(links, results):
-        formatted_results.append(dict(link, **result["data"]["attributes"]["stats"]))
-
-    return formatted_results
-
-
 # VirusTotalThoroughScanner scans a URL and its direct links, aggregates the results, and returns them.
 class VirusTotalThoroughScanner:
     def __init__(self, scanner):
@@ -28,6 +19,24 @@ class VirusTotalThoroughScanner:
         results = await asyncio.gather(*scan_tasks)
 
         # Create a list of dictionaries with link and result
-        formatted_results = [{"link": link, "result": result} for link, result in zip(links, results)]
+        tabular_form = format_to_tabular_form(links, results)
 
-        return formatted_results
+        return tabular_form
+
+
+def format_to_tabular_form(links, results):
+    formatted_results = []
+
+    for link, result in zip(links, results):
+        d = {"link": link} | result["data"]["attributes"]["stats"]
+        formatted_results.append(d)
+
+    return formatted_results
+
+
+class StubScanner:
+    async def scan_url(self, url):
+        return [
+            {"malicious": 2, "suspicious": 6, "undetected": 1, "timeout": 0, "link": "foo.com"},
+            {"malicious": 4, "suspicious": 3, "undetected": 2, "timeout": 7, "link": "boo.com"}
+        ]
